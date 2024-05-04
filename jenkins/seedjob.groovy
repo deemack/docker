ec86a5a7787c36585c8cd783925f2f89a78271fb
David Mackintosh <dmac0010@student.monash.edu>
2024-05-04 20:33:20 +1000

    Update seedjob with test repo https

--- jenkins/seedjob.groovy
+++ jenkins/seedjob.groovy
@@ -1,52 +1,39 @@
 #!/usr/bin/env groovy
-import groovy.io.FileType
 
 /**
- * Setup jobs from gitlab project which contains multiple Jenkinsfiles in different repositories.
+ * Setup default cicd job from src/cicd/Jenkinsfile folder of gitlab project.
  * Add new repositories to list at the end of the file.
- * @param repo
+ * @param repo The repository for which jobs are created
+ * @param path The path to the Jenkinsfile (defaults to cicd)
  * @return
  */
-def createPipelineJobs(final String repo) {
+def createPipelineJob(final String repo, final String path = 'cicd') {
 
-    final String tempDir="/var/jenkins_home/tmp/repos"
     final String groupName = repo.substring(0, repo.lastIndexOf("/"))
     final String repoName = repo.substring(repo.lastIndexOf("/") + 1, repo.length())
+    final String jobName = groupName + "___" + repoName + "___" + path.replace("/", "___")
 
-    println "[INFO] Clone repo '${repo}' and checkout branch 'main'"
-    ["git", "clone", "git@gitlab.com:${repo}.git"].execute()
-    ["git", "checkout", "main"].execute()
-
-    println "[INFO] Wait 30 seconds for git clone to finish"
-    sleep(30000)
-
-    // Iterate folders containing the Jenkinsfiles and create pipeline jobs
-    new File('test/jobs').eachFile FileType.DIRECTORIES, {
-        String folderName = it.name
-        String jobName = groupName + "___" + repoName + "___" + folderName
-
-        pipelineJob(jobName) {
-            definition {
-                cpsScm {
-                    scm {
-                        git {
-                            remote {
-                                url("git@gitlab.com:" + repo +".git")
-                            }
-
-                            branches("*/main")
-                            //branches("*/feat*")
+    pipelineJob(jobName) {
+        definition {
+            cpsScm {
+                scm {
+                    git {
+                        remote {
+                            url("https://github.com/deemack/" + repo +".git")
                         }
-                    }
 
-                    scriptPath("jobs/${folderName}/Jenkinsfile")
+                        branches("*/main")
+                    }
                 }
+
+                scriptPath("src/${path}/Jenkinsfile")
             }
         }
-
     }
-
 }
+
+createPipelineJob("deemack/test", "jobs")
+
 
 //createPipelineJobs("sommerfeld.sebastian/docker-jenkins-pipelines")
 createPipelineJobs("deemack/test")
